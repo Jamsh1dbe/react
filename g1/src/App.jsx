a24da9066bc2259411b7d93910ba9feaa6f5d2ec
JamShidBek2003 <92529675+Jamsh1dbe@users.noreply.github.com>
2021-12-01 13:21:38 +0500

    test

---
+++ g1/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+class Jamshidbek extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>hello Jamshidbek</h1>
+    </div>
+  );
+}
+}
+class Ahrorbek extends React.Component {
+    render() {
+      return (
+      <div>
+        <h1>hello Ahrorbek</h1>
+      </div>
+    );
+  }
+  }
+
+export {Jamshidbek}
+export default Ahrorbek;
